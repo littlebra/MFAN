cbe939cedbc400d9f22170029f6d282f93a0c100
littlebra <1013275219@qq.com>
2020-03-27 16:54:29 +0800

    第一次提交MFAN

---
+++ static/admin/js/main.js
@@ -0,0 +1,170 @@
+layui.use(['layer', 'form', 'element', 'jquery', 'dialog', 'table'], function() {
+	var layer = layui.layer;
+	var element = layui.element;
+	var form = layui.form;
+	var table = layui.table; //表格
+	var $ = layui.jquery;
+	var dialog = layui.dialog;
+	var hideBtn = $('#hideBtn');
+	var mainLayout = $('#main-layout');
+	var mainMask = $('.main-mask');
+	//监听导航点击
+	element.on('nav(leftNav)', function(elem) {
+		// var navA = $(elem).find('a');
+		var id = elem.attr('data-id');
+		var url = elem.attr('data-url');
+		var text = elem.attr('data-text');
+		console.log(elem);
+		console.log(id,text,url);
+		if(!url){
+			return;
+		}
+		var isActive = $('.main-layout-tab .layui-tab-title').find("li[lay-id=" + id + "]");
+		if(isActive.length > 0) {
+			//切换到选项卡
+			element.tabChange('tab', id);
+		} else {
+			element.tabAdd('tab', {
+				title: text,
+				content: '<iframe src="' + url + '" name="iframe' + id + '" class="iframe" framborder="0" data-id="' + id + '" scrolling="auto" width="100%"  height="100%"></iframe>',
+				id: id
+			});
+			element.tabChange('tab', id);
+			
+		}
+		mainLayout.removeClass('hide-side');
+	});
+	//监听导航点击
+	element.on('nav(rightNav)', function(elem) {
+		var navA = $(elem).find('a');
+		var id = navA.attr('data-id');
+		var url = navA.attr('data-url');
+		var text = navA.attr('data-text');
+		if(!url){
+			return;
+		}
+		var isActive = $('.main-layout-tab .layui-tab-title').find("li[lay-id=" + id + "]");
+		if(isActive.length > 0) {
+			//切换到选项卡
+			element.tabChange('tab', id);
+		} else {
+			element.tabAdd('tab', {
+				title: text,
+				content: '<iframe src="' + url + '" name="iframe' + id + '" class="iframe" framborder="0" data-id="' + id + '" scrolling="auto" width="100%"  height="100%"></iframe>',
+				id: id
+			});
+			element.tabChange('tab', id);
+			
+		}
+		mainLayout.removeClass('hide-side');
+	});
+	//菜单隐藏显示
+	hideBtn.on('click', function() {
+		if(!mainLayout.hasClass('hide-side')) {
+			mainLayout.addClass('hide-side');
+		} else {
+			mainLayout.removeClass('hide-side');
+		}
+	});
+	//遮罩点击隐藏
+	mainMask.on('click', function() {
+		mainLayout.removeClass('hide-side');
+	})
+
+	//示范一个公告层
+//	layer.open({
+//		  type: 1
+//		  ,title: false //不显示标题栏
+//		  ,closeBtn: false
+//		  ,area: '300px;'
+//		  ,shade: 0.8
+//		  ,id: 'LAY_layuipro' //设定一个id，防止重复弹出
+//		  ,resize: false
+//		  ,btn: ['火速围观', '残忍拒绝']
+//		  ,btnAlign: 'c'
+//		  ,moveType: 1 //拖拽模式，0或者1
+//		  ,content: '<div style="padding: 50px; line-height: 22px; background-color: #393D49; color: #fff; font-weight: 300;">后台模版1.1版本今日更新：<br><br><br>数据列表页...<br><br>编辑删除弹出功能<br><br>失去焦点排序功能<br>数据列表页<br>数据列表页<br>数据列表页</div>'
+//		  ,success: function(layero){
+//		    var btn = layero.find('.layui-layer-btn');
+//		    btn.find('.layui-layer-btn0').attr({
+//		      href: 'http://www.layui.com/'
+//		      ,target: '_blank'
+//		    });
+//		  }
+//		});
+		  //监听Tab切换
+  element.on('tab(demo)', function(data){
+    layer.tips('切换了 '+ data.index +'：'+ this.innerHTML, this, {
+      tips: 1
+    });
+  });
+  
+  //执行一个 table 实例
+  table.render({
+    elem: '#demo'
+    ,height: 420
+    ,url: '/demo/table/user/' //数据接口
+    ,title: '用户表'
+    ,page: true //开启分页
+    ,toolbar: 'default' //开启工具栏，此处显示默认图标，可以自定义模板，详见文档
+    ,totalRow: true //开启合计行
+    ,cols: [[ //表头
+      {type: 'checkbox', fixed: 'left'}
+      ,{field: 'id', title: 'ID', width:80, sort: true, fixed: 'left', totalRowText: '合计：'}
+      ,{field: 'username', title: '用户名', width:80}
+      ,{field: 'experience', title: '积分', width: 90, sort: true, totalRow: true}
+      ,{field: 'sex', title: '性别', width:80, sort: true}
+      ,{field: 'score', title: '评分', width: 80, sort: true, totalRow: true}
+      ,{field: 'city', title: '城市', width:150} 
+      ,{field: 'sign', title: '签名', width: 200}
+      ,{field: 'classify', title: '职业', width: 100}
+      ,{field: 'wealth', title: '财富', width: 135, sort: true, totalRow: true}
+      ,{fixed: 'right', width: 165, align:'center', toolbar: '#barDemo'}
+    ]]
+  });
+  
+  //监听头工具栏事件
+  table.on('toolbar(test)', function(obj){
+    var checkStatus = table.checkStatus(obj.config.id)
+    ,data = checkStatus.data; //获取选中的数据
+    switch(obj.event){
+      case 'add':
+        layer.msg('添加');
+      break;
+      case 'update':
+        if(data.length === 0){
+          layer.msg('请选择一行');
+        } else if(data.length > 1){
+          layer.msg('只能同时编辑一个');
+        } else {
+          layer.alert('编辑 [id]：'+ checkStatus.data[0].id);
+        }
+      break;
+      case 'delete':
+        if(data.length === 0){
+          layer.msg('请选择一行');
+        } else {
+          layer.msg('删除');
+        }
+      break;
+    };
+  });
+  
+  //监听行工具事件
+  table.on('tool(test)', function(obj){ //注：tool 是工具条事件名，test 是 table 原始容器的属性 lay-filter="对应的值"
+    var data = obj.data //获得当前行数据
+    ,layEvent = obj.event; //获得 lay-event 对应的值
+    if(layEvent === 'detail'){
+      layer.msg('查看操作');
+    } else if(layEvent === 'del'){
+      layer.confirm('真的删除行么', function(index){
+        obj.del(); //删除对应行（tr）的DOM结构
+        layer.close(index);
+        //向服务端发送删除指令
+      });
+    } else if(layEvent === 'edit'){
+      layer.msg('编辑操作');
+    }
+  });
+
+});
